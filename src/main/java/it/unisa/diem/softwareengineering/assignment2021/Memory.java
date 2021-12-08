c3f7a722d0acc9a71c436822359996688bd69820
SimoneSalzano <salzanosimone99@gmail.com>
2021-12-08 16:56:43 +0100

    Applied observable on memory

--- src/main/java/it/unisa/diem/softwareengineering/assignment2021/Memory.java
+++ src/main/java/it/unisa/diem/softwareengineering/assignment2021/Memory.java
@@ -3,10 +3,12 @@ import java.util.*;
 
 
 /** 
- * A stack that manages complex numbers.
- * @Author Dario Montervino 
+ * A stack that manages complex numbers, which can be observed.
+ * It extends Observable, so it can be observed by any class interested in keeping up to date with 
+ * the state of the memory.
+ * @Authors  Simone Salzano, Michele Spremulli, Dario Montervino 
  */
-public class Memory {
+public class Memory extends Observable{
     private Deque<ComplexNumber> stack;
 
     public Memory(){
@@ -15,11 +17,14 @@ public class Memory {
 
     
     /** 
-     * Remove the last element in the memory and return it (LIFO) 
+     * Remove the last element in the memory and return it (LIFO), then notify observers.
      * @return ComplexNumber
      */
     public ComplexNumber pop(){
-        return this.stack.pop();
+        ComplexNumber poppedValue = this.stack.pop();
+        setChanged();
+        notifyObservers();
+        return poppedValue;
     }
 
     
@@ -31,11 +36,13 @@ public class Memory {
     }
 
     
-    /** Insert an element in the memory
+    /** Insert an element in the memory, then notifies the observers
      * @param complex1
      */
     public void push(ComplexNumber complex1){
         this.stack.push(complex1);
+        setChanged();
+        notifyObservers();
     }
 
     
@@ -65,14 +72,16 @@ public class Memory {
     }
     
     /**
-     * Remove all the elements from the stacks
+     * Remove all the elements from the stacks, then notifies the observers
      */ 
     public void clear(){
         this.stack.removeAll(stack);
+        setChanged();
+        notifyObservers();
     }
     
     /**
-     * Duplicate the last element in the stack
+     * Duplicate the last element in the stack, then notifies the observers
      * @throws NotEnoughOperatorsException
      */
     public void dup() throws NotEnoughOperatorsException{
@@ -81,9 +90,10 @@ public class Memory {
         }
         else
             throw new NotEnoughOperatorsException("Drop requires one operand!");
+        notifyObservers();
     }
     /**
-     * Delete the last element in the stack
+     * Delete the last element in the stack, then notifies the observers
      * @throws NotEnoughOperatorsException
      */
     public void drop() throws NotEnoughOperatorsException{
@@ -94,7 +104,7 @@ public class Memory {
             throw new NotEnoughOperatorsException("Drop requires one operand!");
     }
     /**
-     * Duplicate the penultimate element from the stack
+     * Duplicate the penultimate element from the stack, then notifies the observers
      * @throws NotEnoughOperatorsException
      */
     public void over() throws NotEnoughOperatorsException{
@@ -109,7 +119,7 @@ public class Memory {
             throw new NotEnoughOperatorsException("Swap requires two operands!");
     }
     /**
-     * Swap the last element and penultimate element
+     * Swap the last element and penultimate element, then notifies the observers
      * @throws NotEnoughOperatorsException
      */
     public void swap() throws NotEnoughOperatorsException{
